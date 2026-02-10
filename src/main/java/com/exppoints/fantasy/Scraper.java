ba5fb6ed9428e62dae66d0ffce11f96980e18505
ben <ben.holmes1@ucalgary.ca>
2026-02-10 13:01:42 -0800

    starting to add web scraping...

---
+++ src/main/java/com/exppoints/fantasy/Scraper.java
@@ -0,0 +1,8 @@
+package com.exppoints.fantasy;
+
+import org.htmlunit.WebClient;
+import org.htmlunit.html.HtmlPage;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
